2576f386fa4fef057f91705c19eb9cbd80d8ac1a
felixneussel <felix.n98@gmx.de>
2021-12-14 17:05:46 +0100

    scalable test setting

---
+++ performance_check.py
@@ -0,0 +1,50 @@
+import os
+import re
+from Parsers.file_reader import mps_aux_reader
+import numpy as np
+
+def getProblems(directory):
+    files = os.listdir(directory)
+    files = list(filter(lambda x: True if re.match(r'.*\.mps$',x) else False,files))
+    files = list(map(lambda x: re.sub(r'.mps','',x),files))
+    all_approaches = set([])
+    for f in files:
+        for algo in ['MT','MT-K','MT-K-F','MT-K-F-W','ST','ST-K','ST-K-C','ST-K-C-S']:
+            all_approaches.add(f"{f} {algo}")
+    solved = set([])
+    with open('/Users/felixneussel/Library/Mobile Documents/com~apple~CloudDocs/Documents/Uni/Vertiefung/Bachelorarbeit/Implementierung/MIQP_QP_Solver/Results/solved.txt','r') as f:
+        for line in f:
+            solved.add(re.sub(r'\n','',line))
+    unsolved = all_approaches - solved
+    return list(unsolved)
+
+def runPerformanceTest(DIRECTORY,PROBLEMS,TIME_LIMIT):
+    for problem in PROBLEMS:
+        problem, algo = problem.split()
+        problem_data = getProblemData(DIRECTORY,problem)
+
+def getProblemData(DIRECTORY,problem):
+    mps_pa = f"{DIRECTORY}/{problem}.mps"
+    aux_pa = f"{DIRECTORY}/{problem}.aux"
+    n_I,n_R,n_y,m_u,m_l,c_u,d_u,A,B,a,int_lb,int_ub,d_l,C,D,b = mps_aux_reader(mps_pa,aux_pa)
+    H,G_u,G_l = quadraticTerms(n_I,n_R,n_y,c_u,d_u,d_l)
+     
+def quadraticTerms(n_I,n_R,n_y,c_u,d_u,d_l):
+    #Input data
+    np.random.seed(3)
+    H = np.random.normal(loc = 1,size=(n_I+n_R,n_I+n_R))
+    H = H.T@H
+    G_u = np.random.normal(loc = 1,size=(n_y,n_y))
+    G_u = G_u.T@G_u
+    G_l = np.random.normal(loc = 1,size=(n_y,n_y))
+    G_l = G_l.T@G_l 
+
+    return H, G_u, G_l
+
+if __name__ == '__main__':
+    DIRECTORY = '/Users/felixneussel/Documents/Uni/Vertiefung/Bachelorarbeit/Problemdata/data_for_MPB_paper/miplib3conv'
+    PROBLEMS_TO_SOLVE = getProblems(DIRECTORY)
+
+    
+
+    
