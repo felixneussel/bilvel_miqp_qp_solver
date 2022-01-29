37669beafee810fbead97d43d723b1a02d2ef804
felixneussel <felix.n98@gmx.de>
2022-01-29 11:17:33 +0100

    prepared benchmark test

--- Functional/benchmarks.py
+++ Functional/benchmarks.py
@@ -1,7 +1,7 @@
 from Functional.problems import setup_meta_data, setup_master
 from gurobipy import GRB,tuplelist
 import gurobipy as gp
-from numpy import concatenate,diag,array,identity
+from numpy import concatenate,diag,array,identity,infty
 from scipy.linalg import block_diag
 
 def setup_kkt_miqp(problem_data,M):
@@ -32,15 +32,12 @@ def setup_kkt_miqp(problem_data,M):
     model.addMConstr(A=GD,x=y_lambda,sense='=',b=d_l)
 
     CDM = concatenate((C,D,-diag(array([M]*m_l))),axis=1)
-    #model.addMConstr(A=CDM,x=x_I.tolist()+y.tolist()+v.tolist(),sense='<=',b=b)
+    model.addMConstr(A=CDM,x=x_I.tolist()+y.tolist()+v.tolist(),sense='<=',b=b)
 
     IM = concatenate((identity(m_l),diag(array([M]*m_l))),axis=1)
-    #model.addMConstr(A=IM,x=dual.tolist()+v.tolist(),sense="<=",b=array([M]*m_l))
-    model.addMConstr(concatenate((identity(m_l),-C,-D),axis=1),v.tolist()+x_I.tolist()+y.tolist(),"=",-b)
-    v_list = v.tolist()
-    for i, var in enumerate(dual.tolist()):
-        model.addSOS(GRB.SOS_TYPE1,[var,v_list[i]])
+    model.addMConstr(A=IM,x=dual.tolist()+v.tolist(),sense="<=",b=array([M]*m_l))
     return model
+
 
 def setup_sd_miqcpcp(problem_data,big_M,optimized_binary_expansion):
     _,_,_,_,_,_,_,G_l,_,_,d_l,_,_,_,_,_,_,_,b = problem_data
@@ -62,5 +59,9 @@ def optimize_benchmark(approach,time_limit,problem_data,big_M,optimized_binary_e
         raise ValueError(f"Approach {approach} is not valid.")
     model.setParam(GRB.Param.TimeLimit,time_limit)
     model.optimize()
-    return model.status,model.ObjVal,model.Runtime,model.MIPGap
+    try:
+        ObjVal = model.ObjVal
+    except AttributeError:
+        ObjVal = infty
+    return model.status,ObjVal,model.Runtime,model.MIPGap
 
